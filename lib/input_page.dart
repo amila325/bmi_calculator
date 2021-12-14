b6e806465bfb97c3f668953c58d6d512baa9dffc
amila325 <67789160+amila325@users.noreply.github.com>
2021-12-14 23:57:47 +1100

    Card child re usable widget
    Adding re usable widget for card internal widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const reusableCardBackgroundColor = Color(0xFF1D1E33);
@@ -26,11 +27,13 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     containerColor: reusableCardBackgroundColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     containerColor: reusableCardBackgroundColor,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                   ),
                 ),
               ],
@@ -78,10 +81,39 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+
+  IconContent({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80.0),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.containerColor});
+  ReusableCard({required this.containerColor, this.cardChild});
 
   final Color containerColor;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -91,6 +123,7 @@ class ReusableCard extends StatelessWidget {
         color: containerColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
       // child: TextButton.icon(
       //   onPressed: () {},
       //   icon: Text("Next"),
